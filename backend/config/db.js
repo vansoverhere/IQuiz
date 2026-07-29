e2f4373988135d809868932361eadbd7cfc78b37
Vanshika Sharma <vanshika.1882.11.c@gmail.com>
2026-07-29 20:40:03 +0530

    Use environment variables for MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB=async ()=>{
-    await mongoose.connect("mongodb+srv://vanshika188211c_db_user:XxLP5uxmOAS3PGrs@cluster0.kdarxsx.mongodb.net/IQuiz")
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log("DB CONNECTED")
     })
